53ac237aa3f398d31ca847f43b906ba1f452c96b
tbjones1612 <tbjonesphoto@gmail.com>
2024-11-25 14:52:31 -0500

    URL redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
 import Maps from './pages/Maps';
@@ -10,16 +9,17 @@ import './App.css';
 
 const App: React.FC = () => {
   return (
-    <Router>
+    // Add basename here for compatibility with GitHub Pages
+    <Router basename="/">
       <div className="app-container">
         {/* Top Header */}
         <header className="top-header stripe">
           <div className="left-box"></div>
-          <svg className='left-curve' xmlns="http://www.w3.org/2000/svg" viewBox="-2 0 13 12">
+          <svg className="left-curve" xmlns="http://www.w3.org/2000/svg" viewBox="-2 0 13 12">
             <path d="M -2 0 L 11 -0 C 10 -0 8 -0 7 4 L 6 8 C 5 12 3 12 2 12 L -2 12 L -2 0" />
           </svg>
           <div className="e-m-title"><h1>ElectionMap.lol</h1></div>
-          <svg className='right-curve' xmlns="http://www.w3.org/2000/svg" viewBox="-11 -12 13 12">
+          <svg className="right-curve" xmlns="http://www.w3.org/2000/svg" viewBox="-11 -12 13 12">
             <path d="M 2 0 L -11 0 C -10 0 -8 0 -7 -4 L -6 -8 C -5 -12 -3 -12 -2 -12 L 2 -12 L 2 0" />
           </svg>
           <div className="right-box"></div>
@@ -27,10 +27,10 @@ const App: React.FC = () => {
 
         {/* Second Header for Navigation */}
         <header className="nav-header">
-          <Link to="/maps"><div className='nav-button'><p>Maps</p></div></Link>
-          <Link to="/polls"><div className='nav-button'><p>Polls</p></div></Link>
-          <Link to="/graphs"><div className='nav-button'><p>Graphs</p></div></Link>
-          <Link to="/about"><div className='nav-button'><p>About</p></div></Link>
+          <Link to="/maps"><div className="nav-button"><p>Maps</p></div></Link>
+          <Link to="/polls"><div className="nav-button"><p>Polls</p></div></Link>
+          <Link to="/graphs"><div className="nav-button"><p>Graphs</p></div></Link>
+          <Link to="/about"><div className="nav-button"><p>About</p></div></Link>
         </header>
 
         {/* Main Content */}
@@ -48,6 +48,6 @@ const App: React.FC = () => {
       </div>
     </Router>
   );
-}
+};
 
 export default App;
